7e0be2bd60b796bd3449038632f83fb058dc5b0a
Sarra Lejmi <sarra.lejmi2@gmail.com>
2023-03-06 15:37:53 +0100

    step-25

--- lib/services/auth/bloc/auth_state.dart
+++ lib/services/auth/bloc/auth_state.dart
@@ -1,3 +1,4 @@
+import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart' show immutable;
 import 'package:mynotes/services/auth/auth_user.dart';
 
@@ -6,26 +7,30 @@ abstract class AuthState {
   const AuthState();
 }
 
-class AuthStateLoading extends AuthState {
-  const AuthStateLoading();
+class AuthStateUninitialized extends AuthState {
+  const AuthStateUninitialized();
+}
+
+class AuthStateRegistering extends AuthState {
+  final Exception? exception;
+
+  const AuthStateRegistering(this.exception);
 }
 
 class AuthStateLoggedIn extends AuthState {
-  final AuthUser  user;
+  final AuthUser user;
   const AuthStateLoggedIn(this.user);
 }
 
-class AuthStateNeedsVerification extends AuthState  {
+class AuthStateNeedsVerification extends AuthState {
   const AuthStateNeedsVerification();
 }
 
-class AuthStateLoggedOut extends AuthState {
+class AuthStateLoggedOut extends AuthState with EquatableMixin {
   final Exception? exception;
-
-  const AuthStateLoggedOut(this.exception);
-}
-
-class AuthStateLogoutFailure extends AuthState {
-  final Exception exception;
-  const AuthStateLogoutFailure(this.exception);
+  final bool isLoading;
+  const AuthStateLoggedOut({required this.exception, required this.isLoading});
+  
+  @override
+  List<Object?> get props => [exception, isLoading];
 }
