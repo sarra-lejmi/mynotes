ca629e7b15d58f9b04192c65677f3f54ca629882
Sarra Lejmi <sarra.lejmi2@gmail.com>
2023-03-07 12:43:22 +0100

    step-28

--- lib/views/forgot_password_view.dart
+++ lib/views/forgot_password_view.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mynotes/services/auth/bloc/auth_bloc.dart';
 import 'package:mynotes/services/auth/bloc/auth_event.dart';
