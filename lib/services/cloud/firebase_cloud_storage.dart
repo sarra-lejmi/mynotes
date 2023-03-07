9bfce37be54c69b3a8bd035d0790890e680f2587
Sarra Lejmi <sarra.lejmi2@gmail.com>
2023-03-07 14:22:48 +0100

    step-29

--- lib/services/cloud/firebase_cloud_storage.dart
+++ lib/services/cloud/firebase_cloud_storage.dart
@@ -20,24 +20,14 @@ class FirebaseCloudStorage {
         documentId: fetchedNote.id, ownerUserId: ownerUserId, text: "");
   }
 
-  Future<Iterable<CloudNote>> getNotes({required String ownerUserId}) async {
-    try {
-      return await notes.where(ownerUserId, isEqualTo: ownerUserId).get().then(
-            (value) => value.docs.map((doc) => CloudNote.fromSnapshot(doc)),
-          );
-    } catch (e) {
-      throw CouldNotGetAllNotesException();
-    }
+  Stream<Iterable<CloudNote>> allNotes({required String ownerUserId}) {
+    final allNotes = notes
+        .where(ownerUserIdFieldName, isEqualTo: ownerUserId)
+        .snapshots()
+        .map((event) => event.docs.map((doc) => CloudNote.fromSnapshot(doc)));
+        
+    return allNotes;
   }
-
-  Stream<Iterable<CloudNote>> allNotes({required String ownerUserId}) =>
-      notes.snapshots().map(
-            (event) => event.docs
-                .map(
-                  (doc) => CloudNote.fromSnapshot(doc),
-                )
-                .where((note) => note.ownerUserId == ownerUserId),
-          );
 
   Future<void> updateNote({
     required String documentId,
